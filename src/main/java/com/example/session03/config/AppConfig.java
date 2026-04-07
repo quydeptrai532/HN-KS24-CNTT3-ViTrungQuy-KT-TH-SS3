369ee73431ede6be3c6466c5d29ca5f92e2bff8d
quydeptrai532 <viquy16@gmail.com>
2026-04-07 13:45:04 +0700

    a

--- src/main/java/com/example/session03/config/AppConfig.java
+++ src/main/java/com/example/session03/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.example.session03.config; // Đổi lại đúng tên package của bạn
+package com.example.session03.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,13 +9,12 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.example.session03") // Trỏ vào package gốc của bạn
+@ComponentScan(basePackages = "com.example.session03")
 public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        // Trỏ đúng vào thư mục chứa views như trong ảnh của bạn
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         return resolver;
